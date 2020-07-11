35be7d62ff4b65cb5dc0528186408bed902da5b4
jaspreet kaur <jaspreet@supertal.io>
2020-07-12 00:05:29 +0530

    image picker = removed custom button

--- App.js
+++ App.js
@@ -12,7 +12,6 @@ import ImagePicker from 'react-native-image-picker';
 
 const options = {
   title: 'Select Avatar',
-  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
   storageOptions: {
     skipBackup: true,
     path: 'images',
